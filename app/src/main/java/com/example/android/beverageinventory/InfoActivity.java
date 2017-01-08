a5460e5c689fa46eb465750ba04c3509211d62f5
EzraYang <deleguo@gmail.com>
2017-01-08 22:04:44 +0800

    1-8-2 simple visual polish and intent

--- app/src/main/java/com/example/android/beverageinventory/InfoActivity.java
+++ app/src/main/java/com/example/android/beverageinventory/InfoActivity.java
@@ -2,6 +2,7 @@ package com.example.android.beverageinventory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_info, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/android/beverageinventory/MainActivity.java
+++ app/src/main/java/com/example/android/beverageinventory/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.beverageinventory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
         // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_add:
+                // jump to @InfoActivity
+                Intent addProduct = new Intent(this, InfoActivity.class);
+                startActivity(addProduct);
+                return true;
+            default:
+                Log.e(LOG_TAG, "Problem jumping to InfoActivity");
+                return false;
+        }
     }
 
 }
