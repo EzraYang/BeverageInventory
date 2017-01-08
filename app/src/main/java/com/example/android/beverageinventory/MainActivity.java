d53c5a1b02e6f63f9c4eb679880473613e66b02a
EzraYang <deleguo@gmail.com>
2017-01-08 20:29:34 +0800

    1-8-1 done UI

--- app/src/main/java/com/example/android/beverageinventory/MainActivity.java
+++ app/src/main/java/com/example/android/beverageinventory/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.beverageinventory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
